9f74e465d9431cad62783b78c3e46974aa09fafc
Bojan Cakić <cakicbojan@gmail.com>
2020-11-30 18:01:07 +0100

    Osnova spring projekta

---
+++ XmlProjekat/src/main/java/ftn/xml/XmlProjekat/XmlProjekatApplication.java
@@ -0,0 +1,13 @@
+package ftn.xml.XmlProjekat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmlProjekatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XmlProjekatApplication.class, args);
+	}
+
+}
---
+++ XmlProjekat/src/test/java/ftn/xml/XmlProjekat/XmlProjekatApplicationTests.java
@@ -0,0 +1,13 @@
+package ftn.xml.XmlProjekat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XmlProjekatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
