616080f91e692d8d38826d4c36de7478b95f2af1
milos012 <milos441@gmail.com>
2020-12-07 21:54:02 +0100

    dodat model za zalbu na odluku i doradjena sema

---
+++ XmlProjekat/src/main/java/ftn/xml/XmlProjekat/model/zalbanaodluku/Zaglavlje.java
@@ -0,0 +1,37 @@
+package ftn.xml.XmlProjekat.model.zalbanaodluku;
+
+import javax.xml.bind.annotation.XmlElement;
+
+public class Zaglavlje {
+    
+    private String naslov;
+    private String primalac;
+    private String adresaPrimaoca;
+
+    public String getNaslov() {
+        return naslov;
+    }
+
+    @XmlElement(name = "naslov")
+    public void setNaslov(String naslov) {
+        this.naslov = naslov;
+    }
+
+    public String getPrimalac() {
+        return primalac;
+    }
+
+    @XmlElement(name = "primalac")
+    public void setPrimalac(String primalac) {
+        this.primalac = primalac;
+    }
+
+    public String getAdresaPrimaoca() {
+        return adresaPrimaoca;
+    }
+
+    @XmlElement(name = "adresa_primaoca")
+    public void setAdresaPrimaoca(String adresaPrimaoca) {
+        this.adresaPrimaoca = adresaPrimaoca;
+    }
+}
---
+++ XmlProjekat/src/main/java/ftn/xml/XmlProjekat/model/zalbanaodluku/ZalbaSadrzaj.java
@@ -0,0 +1,67 @@
+package ftn.xml.XmlProjekat.model.zalbanaodluku;
+
+import javax.xml.bind.annotation.XmlElement;
+
+public class ZalbaSadrzaj {
+    
+    private TPodnosilacZalbe podnosilacZalbe;
+    private String naziv_organa_vlasti;
+    private String broj_zalbe;
+    private String godina;
+    private Paragrafi paragrafi;
+    private Podaci podaci;
+
+    public TPodnosilacZalbe getPodnosilacZalbe() {
+        return podnosilacZalbe;
+    }
+
+    @XmlElement(name = "podnosilac_zalbe")
+    public void setPodnosilacZalbe(TPodnosilacZalbe podnosilacZalbe) {
+        this.podnosilacZalbe = podnosilacZalbe;
+    }
+
+    public String getNazivOrganaVlasti() {
+        return naziv_organa_vlasti;
+    }
+
+    @XmlElement(name = "naziv_organa_vlasti")
+    public void setNazivOrganaVlasti(String naziv_organa_vlasti) {
+        this.naziv_organa_vlasti = naziv_organa_vlasti;
+    }
+
+    public String getBrojZalbe() {
+        return broj_zalbe;
+    }
+
+    @XmlElement(name = "broj_zalbe")
+    public void setBrojZalbe(String broj_zalbe) {
+        this.broj_zalbe = broj_zalbe;
+    }
+
+    public String getGodina() {
+        return godina;
+    }
+
+    @XmlElement(name = "godina")
+    public void setGodina(String godina) {
+        this.godina = godina;
+    }
+
+    public Paragrafi getParagrafi(){
+        return paragrafi;
+    }
+
+    @XmlElement(name = "paragrafi")
+    public void setParagrafi(Paragrafi paragrafi) {
+        this.paragrafi = paragrafi;
+    }
+
+    public Podaci getPodaci(){
+        return podaci;
+    }
+
+    @XmlElement(name = "paragrafi")
+    public void setPodaci(Podaci podaci) {
+        this.podaci = podaci;
+    }
+}
